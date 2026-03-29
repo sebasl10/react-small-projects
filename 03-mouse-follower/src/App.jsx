a7141c5f5b37a30afea502acdb175abe919d8182
Sebastian Linares <juanselindel@gmail.com>
2026-03-29 11:43:47 +0200

    lint

--- 03-mouse-follower/src/App.jsx
+++ 03-mouse-follower/src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
-import { MouseFollower } from './components/MouseFollower'
+import "./App.css";
+import { MouseFollower } from "./components/MouseFollower";
 
 function App() {
-
   return (
     <main>
       <MouseFollower />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- 03-mouse-follower/src/components/MouseFollower.jsx
+++ 03-mouse-follower/src/components/MouseFollower.jsx
@@ -1,49 +1,59 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-export function MouseFollower(){
-  const [enabled, setEnabled] = useState(false)
-  const [position, setPosition] = useState({ x: -20, y: -20 })
+export function MouseFollower() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: -20, y: -20 });
 
   useEffect(() => {
-    console.log('Effect : ', enabled)
+    console.log("Effect : ", enabled);
 
     const handleMove = (event) => {
-      const { clientX, clientY } = event
-      setPosition({ x: clientX, y: clientY })
-      console.log('Move : ', clientX, clientY)
-    }
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+      console.log("Move : ", clientX, clientY);
+    };
 
-    if (enabled){
-      window.addEventListener('pointermove', handleMove)
+    if (enabled) {
+      window.addEventListener("pointermove", handleMove);
     }
 
     return () => {
-      window.removeEventListener('pointermove', handleMove)
-      setPosition({ x: -20, y: -20 })
-    }
-  }, [enabled])
+      window.removeEventListener("pointermove", handleMove);
+      setPosition({ x: -20, y: -20 });
+    };
+  }, [enabled]);
+
+  useEffect(() => {
+    document.body.classList.toggle("no-cursor", enabled);
+
+    return () => {
+      document.body.classList.remove("no-cursor");
+    };
+  }, [enabled]);
 
   const handleClick = () => {
-    setEnabled(!enabled)
-  }
+    setEnabled(!enabled);
+  };
 
   return (
     <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: '#09f',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -20,
-        top: -20,
-        width: 40,
-        height: 40,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}></div>
+      <div
+        style={{
+          position: "absolute",
+          backgroundColor: "#09f",
+          borderRadius: "50%",
+          opacity: 0.8,
+          pointerEvents: "none",
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }}
+      ></div>
       <button onClick={handleClick}>
-        {enabled ? 'Disable' : 'Enable'} Mouse Follower
+        {enabled ? "Disable" : "Enable"} Mouse Follower
       </button>
     </>
-  )
+  );
 }
